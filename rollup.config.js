4578f70602bf4e5fc09123aa671a648d78d94015
Alexander Bespoyasov <forjob159@gmail.com>
2022-06-01 19:01:21 +0200

    Simplify the builder

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,11 @@
-// rollup.config.js
 import vue from "rollup-plugin-vue";
-import buble from "rollup-plugin-buble";
-import replace from "rollup-plugin-replace";
-import uglify from "rollup-plugin-uglify-es";
-import minimist from "minimist";
 
-const argv = minimist(process.argv.slice(2));
-
-const config = {
-  input: "src/entry.js",
+export default {
+  input: "./index.js",
   output: {
-    name: "VueProkrutchik",
-    exports: "named",
+    name: "Scroller",
   },
   plugins: [
-    replace({
-      "process.env.NODE_ENV": JSON.stringify("production"),
-    }),
     vue({
       css: true,
       compileTemplate: true,
@@ -24,15 +13,5 @@ const config = {
         isProduction: true,
       },
     }),
-    buble({
-      objectAssign: "Object.assign",
-    }),
   ],
 };
-
-// Only minify browser (iife) version
-if (argv.format === "iife") {
-  config.plugins.push(uglify());
-}
-
-export default config;
