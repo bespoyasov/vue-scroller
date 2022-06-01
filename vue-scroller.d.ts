54c615de78c23b8e8837aed914a41b4e240aac02
Alexander Bespoyasov <forjob159@gmail.com>
2022-06-01 20:02:38 +0200

    Add TypeScript support

---
+++ vue-scroller.d.ts
@@ -0,0 +1,19 @@
+declare module "vue-scroller" {
+  import * as Vue from "vue";
+  import * as ScrollerBase from "prokrutchik";
+
+  type VueScrollerProps = {
+    position: ScrollerBase.ScrollDestination;
+    duration: ScrollerBase.MilliSecondsCount;
+
+    scrollbar: ScrollerBase.ElementVisibility;
+    navigation: ScrollerBase.ElementVisibility;
+    align: ScrollerBase.ContentAlignment;
+
+    onItemClick: (e: MouseEvent | TouchEvent) => void;
+  };
+
+  export interface VueScrollerConstructor extends Vue.VueConstructor {
+    props: VueScrollerProps;
+  }
+}
